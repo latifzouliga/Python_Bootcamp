f6c00cb1a2c6d8bc38f7b45461ae961bf5e9cab5
Abdellatif Zouliga <zuuliga@gmail.com>
2023-09-19 13:26:12 -0400

    while loop

--- day_02/loops.py
+++ day_02/loops.py
@@ -7,14 +7,37 @@ print("================================")
 for i in range(0, len(s)):
     print(s[i])
 
-
 print("=============== reverse =================")
 
 for i in range(-len(s), 0):
     print(s[i])
 
-
 print("=============== reverse =================")
 
 for i in reversed(range(0, len(s))):
     print(s[i])
+
+print("=============== reverse =================")
+
+result = ''
+for x in s[::-1]:
+    result += x
+print(result)
+
+print("================ while loop =================")
+
+# num = int(input('Enter a Positive number\n'))
+#
+# while num <= 0:
+#     num = int(input('Enter a Positive number\n'))
+#
+# print(f'You have entered: {num}')
+
+print("=================================")
+
+answer = input("Enter yes or No\n")
+
+while not(answer.lower() == 'yes' or answer.lower() == 'no'):
+    answer = input("Enter yes or No\n")
+
+print(f'You have entered {answer}')
