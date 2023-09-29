461dc003f98b8396f4717213a9d2e26d94f9f8b7
Abdellatif Zouliga <zuuliga@gmail.com>
2023-09-28 20:26:19 -0400

    grade_level2

--- practice/grade_level2.py
+++ practice/grade_level2.py
@@ -1,5 +1,6 @@
 """
-14. Create a python file named grade_level2.  write a program that asks user to enter the grade level number,  determine and print which school type the person is in is in.
+14. Create a python file named grade_level2.  write a program that asks user to enter the grade level number,
+determine and print which school type the person is in is in.
             grade level and types are:
                     1-5: Elementary school
                     6-8: Middle school
@@ -11,3 +12,19 @@
 
             NOTE: MUST USE NESTED IF. DO NOT USE MORE THAN ONE PRINT STATEMENT
 """
+
+grade = int(input('Enter you grade\n'))
+
+if 0 < grade <= 18:
+    if grade <= 5:
+        print('Elementary school')
+    elif grade <= 8:
+        print('Middle school')
+    elif grade <= 12:
+        print('High school')
+    elif grade <= 16:
+        print('College')
+    else:
+        print('grand School')
+else:
+    print("invalid")
