ecccb707e3861950ce3cd987d7a4da5cefda375e
Abdellatif Zouliga <zuuliga@gmail.com>
2023-10-14 22:24:20 -0400

    tip calc

--- complete_python_bootcamp/day_02_data_types_and_strings/tip_calculator.py
+++ complete_python_bootcamp/day_02_data_types_and_strings/tip_calculator.py
@@ -8,7 +8,9 @@ print("Welcome to the tip calculator")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 num_of_people = int(input("How many people to split the bill? "))
-calc_tip = bill * (tip / 100)
-bill += calc_tip
+bill += bill * (tip / 100)
+# calc_tip = bill * (tip / 100)
+# bill += calc_tip
 bill /= num_of_people
-print(f"Each person should pay: ${round(bill, 2)}")
+# print(f"Each person should pay: ${round(bill, 2)}")
+print(f"Each person should pay: ${bill:.2f}")
